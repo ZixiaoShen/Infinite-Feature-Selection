52189156614eece2f595a271bb6baa5e79567686
沈子骁 <shenzixiao@chenzixiaodeMacBook-Pro.local>
2019-08-23 22:21:22 +0100

    add information

--- IFS_Zeal.m
+++ IFS_Zeal.m
@@ -15,6 +15,68 @@ function [Rank, Score] = IFS_Zeal(train_X, train_y, alpha, supervision, verbose)
         mu_sample_negative = mean(sample_negative);
         mu_sample_positive = mean(sample_positive);
         priors_corr = ([mu_sample_positive - mu_sample_negative].^2);
-        hello world
         
+        std_postive = std(sample_postive).^2;
+        total_std = std_postive + std(sample_negative).^2;
+        total_std(find(total_std == 0)) = 10000;
+        
+        corr_ij = priors_corr ./ total_std;
+        corr_ij = corr_ij' * corr_ij;
+        
+        corr_ij = corr_ij - min(min(corr_ij));
+        corr_ij = corr_ij ./ max(max(corr_ij));
+        
+    else
+        [corr_ij, pval] = corr(train_X, 'type', 'Spearman');
+        corr_ij(isnan(corr_ij)) = 0; % remove NaN
+        corr_ij(isinf(corr_ij)) = 0; % remove inf
+        corr_ij = 1 - abs(corr_ij);
+        save(filename, 'corr_ij');
+    end
+    
+    % Standard Deviation Estimation
+    STD = std(train_X, [], 1);
+    STDMatrix = bsxfun(@max, STD, STD');
+    STDMatrix = STDMatrix - min(min(STDMatrix));
+    sigma_ij = STDMatrix ./ max(max(STDMatrix));
+    sigma_ij(isnan(sigma_ij)) = 0; % remove NaN
+    sigma_ij(isinf(sigma_ij)) = 0; % remove inf
+    
+    %% 2) Building the graph G = <V, E>
+    if (verbose)
+        fprintf('2) Building the graph G = <V, E> \n');
+    end
+    A = (alpha * corr_ij + (1 - alpha) * sigma_ij);
+    
+    factor = 0.99;
+    
+    %% 3) Letting paths tend to infinite: Inf-FS Core
+    if (verbose)
+        fprintf('3) Letting paths tend to infinite \n');
+    end
+    I = eye(size(A, 1));  % Identity Matrix
+    
+    r = (factor/max(eig(A))); % Set a meaningful value for r
+    
+    y = I - (r * A);
+    
+    S = inv(y) - I; % see Gelfand's formula - convergence of the geometric series of matrices
+    
+    %% 4) Estimating energy scores
+    if (verbose)
+        fprintf('4) Estimating energy scores \n');
+    end
+    
+    Score = sum(S, 2);  % energy scores s(i)
+    
+    %% 5) Ranking features according to s
+    if (verbose)
+        fprintf('5) Features ranking \n');
+    end
+    [~, Rank] = sort(Score, 'descend');
+    
+    Rank = Rank';
+    Score = Score';
+    
 end
+
